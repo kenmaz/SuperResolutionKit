ed98e64f33439ec5e0518297231e67b8f4590e1e
kenmaz <kentaro.matsumae@gmail.com>
2018-07-16 00:29:04 +0900

    add progressbar

--- script/fsrcnn/convert.py
+++ script/fsrcnn/convert.py
@@ -11,6 +11,7 @@ args = parser.parse_args()
 import numpy as np
 from scipy import misc
 from PIL import Image
+from tqdm import tqdm
 
 scale = 2.0
 label_size = 200
@@ -25,13 +26,16 @@ if not exists(join(args.output_dir, "label")):
     makedirs(join(args.output_dir, "label"))
 
 count = 1
-for f in listdir(args.input_dir):
+dirs = listdir(args.input_dir)
+
+for i in tqdm(range(len(dirs))):
+    f = dirs[i]
     f = join(args.input_dir, f)
     if not isfile(f):
         continue
 
     image = np.asarray(Image.open(f).convert('RGB'))
-    print(f, image.shape)
+    #print(f, image.shape)
 
     h, w, c = image.shape
 
@@ -40,7 +44,7 @@ for f in listdir(args.input_dir):
     for y in range(0, h - label_size + 1, stride):
         for x in range(0, w - label_size + 1, stride):
             (x_p, y_p) = (int(x/scale), int(y/scale))
-            print(y,x,y_p,x_p)
+            #print(y,x,y_p,x_p)
             sub_img_patch = scaled[y_p : y_p + patch_size, x_p : x_p + patch_size]
             sub_img_label = image[y : y + label_size, x : x + label_size]
             misc.imsave(join(args.output_dir, "input", str(count) + '.png'), sub_img_patch)
