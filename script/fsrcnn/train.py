f55892ca1edbd1cb8b36651c84fd827443554f04
kenmaz <kentaro.matsumae@gmail.com>
2018-07-17 02:10:09 +0900

    fix

--- script/fsrcnn/train.py
+++ script/fsrcnn/train.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, Conv2DTranspose, Input, BatchNormalization, PReLU
 from keras.callbacks import ModelCheckpoint, Callback, TensorBoard
 from keras.optimizers import SGD, Adam
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import math
 import os
@@ -35,48 +36,27 @@ def model(scale = 2):
     SRCNN.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
     return SRCNN
 
-class MyDataGenerator(object):
-
-    def flow_from_directory(self, input_dir, label_dir, batch_size=32):
-        images = []
-        labels = []
-        while True:
-            files = listdir(input_dir)
-            random.shuffle(files)
-            for f in files:
-                images.append(self.load_image(input_dir, f))
-                labels.append(self.load_image(label_dir, f))
-                if len(images) == batch_size:
-                    x_inputs = np.asarray(images)
-                    x_labels = np.asarray(labels)
-                    images = []
-                    labels = []
-                    yield x_inputs, x_labels
-
-    def load_image(self, src_dir, f):
-        X = np.asarray(Image.open(join(src_dir, f)).convert('RGB'), dtype='float32')
-        X /= 255.
-        return X
-
-def train(log_dir, model_dir, train_dir, test_dir, eval_img, scale):
+def train(log_dir, model_dir, train_dir, test_dir, eval_img, scale, epochs, steps):
     srcnn_model = model(scale)
     print(srcnn_model.summary())
 
-    datagen = MyDataGenerator()
-    train_gen = datagen.flow_from_directory(os.path.join(
-        train_dir, 'input'),
-        os.path.join(train_dir, 'label'),
-        batch_size = 10)
+    datagen = ImageDataGenerator(
+            rescale = 1./255,
+            horizontal_flip = True,
+            vertical_flip = True,
+            zoom_range = [0.6, 1.0])
+
+    train_gen = datagen.flow_from_directory(
+            train_dir,
+            target_size = (100,100),
+            class_mode = 'input',
+            batch_size = 10)
 
     val_gen = datagen.flow_from_directory(
-        os.path.join(test_dir, 'input'),
-        os.path.join(test_dir, 'label'),
-        batch_size = 10)
-
-    class PredCallback(Callback):
-        def on_epoch_end(self, epoch, logs=None):
-            pass
-            #pred.predict(self.model, eval_img, 'base-%d.png' % epoch, 'out-%d.png' % epoch, False)
+            test_dir,
+            target_size = (100,100),
+            class_mode = 'input',
+            batch_size = 10)
 
     class PSNRCallback(Callback):
         def on_epoch_end(self, epoch, logs=None):
@@ -88,7 +68,6 @@ def train(log_dir, model_dir, train_dir, test_dir, eval_img, scale):
             print("PSNR:%s" % psnr)
             print("PSNR(val):%s" % val_psnr)
 
-    pd_cb = PredCallback()
     ps_cb = PSNRCallback()
     md_cb = ModelCheckpoint(os.path.join(model_dir,'check.h5'), monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='min', period=1)
     tb_cb = TensorBoard(log_dir=log_dir)
@@ -96,11 +75,11 @@ def train(log_dir, model_dir, train_dir, test_dir, eval_img, scale):
 
     srcnn_model.fit_generator(
         generator = train_gen,
-        steps_per_epoch = 100,
+        steps_per_epoch = steps,
         validation_data = val_gen,
-        validation_steps = 100,
-        epochs = 100,
-        callbacks=[pd_cb, ps_cb, md_cb, tb_cb, s3_cb])
+        validation_steps = steps,
+        epochs = epochs,
+        callbacks=[ps_cb, md_cb, tb_cb, s3_cb])
 
     srcnn_model.save(os.path.join(model_dir,'model.h5'))
 
@@ -113,10 +92,12 @@ if __name__ == "__main__":
     parser.add_argument("test_dir")
     parser.add_argument("--eval_img")
     parser.add_argument("-scale", type=int, default=2)
+    parser.add_argument("-epochs", type=int, default=100)
+    parser.add_argument("-steps", type=int, default=100)
     args = parser.parse_args()
     print(args)
 
     if not exists(args.model_dir):
         makedirs(args.model_dir)
 
-    train(args.log_dir, args.model_dir, args.train_dir, args.test_dir, args.eval_img, args.scale)
+    train(args.log_dir, args.model_dir, args.train_dir, args.test_dir, args.eval_img, args.scale, args.epochs, args.steps)
