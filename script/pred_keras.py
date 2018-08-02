39aa8242bd95706a55b32e641b453b46486d32c8
kenmaz <kentaro.matsumae@gmail.com>
2018-08-03 00:54:47 +0900

    adopt srcnn to y ch process

---
+++ script/pred_keras.py
@@ -0,0 +1,114 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, Input, BatchNormalization
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import SGD, Adam
+import numpy as np
+import math
+import os
+from keras.models import load_model
+from PIL import Image
+
+patch_size = 200
+
+def setup_session():
+    import tensorflow as tf
+    from keras.backend import tensorflow_backend
+    config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+    session = tf.Session(config=config)
+    tensorflow_backend.set_session(session)
+
+def predict2(model, input_file, out_dir, scale = 2.0, ch = 1):
+    basename = os.path.basename(input_file)
+    filename, ext = os.path.splitext(basename)
+
+    img = Image.open(input_file)
+    img.save('%s/%s_org%s' % (out_dir, filename, ext))
+    img = img.convert('YCbCr')
+
+    yuv = np.asarray(img)
+    y_img = yuv[:,:,0]
+    y_img_shape = y_img.shape
+    y_img = Image.fromarray(y_img)
+    y_img.save('%s/%s_org_y%s' % (out_dir, filename, ext))
+
+    lr_size = tuple([int(x/scale) for x in img.size])
+    lr_img = img.resize(lr_size, Image.BICUBIC)
+    lr_img = lr_img.resize(img.size, Image.BICUBIC)
+    lr_img.resize(img.size, Image.BICUBIC).convert('RGB').save('%s/%s_lr%s' % (out_dir, filename, ext))
+
+    ylr_img = np.asarray(lr_img)[:,:,0]
+    ylr_img = np.uint8(ylr_img)
+    Image.fromarray(ylr_img).resize(img.size, Image.BICUBIC).save('%s/%s_ylr%s' % (out_dir, filename, ext))
+    ylr_img = np.reshape(ylr_img, (ylr_img.shape[0],ylr_img.shape[1],ch))
+
+    y_hr = predict_hr_img(model, ylr_img, y_img_shape, scale, out_dir)
+    y_hr = np.uint8(y_hr)
+    h,w,c = yuv.shape
+    y_hr = y_hr[0:h,0:w]
+    #Image.fromarray(y_hr).convert('RGB').save('%s/%s_hr%s' % (out_dir, filename, ext))
+
+    merge_save('%s/%s_hr_merge%s' % (out_dir, filename, ext), yuv, y_hr)
+
+def predict_hr_img(model, lr_img, hr_img_size, scale, out_dir):
+    hr_img = np.zeros((hr_img_size[1], hr_img_size[0], 1)) # h<->w exchanged
+    print(hr_img.shape)
+    print(lr_img.shape)
+
+    h,w,ch = lr_img.shape
+    eh = h if h % patch_size == 0 else h + patch_size - (h % patch_size) # 240 % 100=40, 240+100-
+    ew = w if w % patch_size == 0 else w + patch_size - (w % patch_size)
+    print('extend:',eh,ew,ch)
+    lr_base = np.zeros((eh, ew, ch), dtype='uint8')
+    lr_base[0:h, 0:w] = lr_img
+    lr_img = lr_base
+    hr_img = np.zeros((int(eh * scale), int(ew * scale), ch)) # h<->w exchanged
+
+    for y in range(0, h, patch_size):
+        for x in range(0, w, patch_size):
+            print y,x
+            patch = lr_img[y:y+patch_size, x:x+patch_size]
+            save_as_img('lr',out_dir, patch, y, x)
+            patch = patch/255.
+            patch = patch.reshape(1, patch.shape[0],patch.shape[1],patch.shape[2])
+            res = model.predict(patch, batch_size=1)
+            res = res*255.
+            res = np.clip(res, 0, 255) #important
+            res = np.uint8(res)
+            res = res.reshape(res.shape[1],res.shape[2],res.shape[3])
+            save_as_img('hr',out_dir, res, y, x)
+            hr_img[y:y+patch_size,x:x+patch_size] = res
+
+    return hr_img
+
+def merge_save(dst_path, yuv, y):
+    y = np.reshape(y, (y.shape[0], y.shape[1]))
+    dst = yuv.copy()
+    dst[:,:,0] = y
+    img = Image.fromarray(dst, mode='YCbCr')
+    img = img.convert('RGB')
+    img.save(dst_path)
+
+def save_as_img(prefix, out_dir, patch, y, x):
+    patch = np.reshape(patch, (patch.shape[0],patch.shape[1]))
+    img = Image.fromarray(patch)
+    path = '%s/%s_patch_%d_%d.png' % (out_dir,prefix,y,x)
+    img.save(path)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="model file path")
+    parser.add_argument("input", help="row res image path")
+    parser.add_argument("out_dir", help="output dir")
+    parser.add_argument("-scale", type=int, default=2)
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
+
+    setup_session()
+    model = load_model(args.model)
+
+    predict2(model, args.input, args.out_dir, args.scale)
+    print('fin')
--- script/train.py
+++ script/train.py
@@ -11,10 +11,11 @@ from os.path import isfile, join, exists
 from PIL import Image
 
 def model():
+    ch = 1
     SRCNN = Sequential()
-    SRCNN.add(Conv2D(nb_filter=128, nb_row=9, nb_col=9, init='glorot_uniform', activation='relu', border_mode='same', bias=True, input_shape=(200, 200, 3)))
+    SRCNN.add(Conv2D(nb_filter=128, nb_row=9, nb_col=9, init='glorot_uniform', activation='relu', border_mode='same', bias=True, input_shape=(200, 200, ch)))
     SRCNN.add(Conv2D(nb_filter=64, nb_row=3, nb_col=3, init='glorot_uniform', activation='relu', border_mode='same', bias=True))
-    SRCNN.add(Conv2D(nb_filter=3, nb_row=5, nb_col=5, init='glorot_uniform', activation='linear', border_mode='same', bias=True))
+    SRCNN.add(Conv2D(nb_filter=ch, nb_row=5, nb_col=5, init='glorot_uniform', activation='linear', border_mode='same', bias=True))
     adam = Adam(lr=0.0003)
     SRCNN.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
     return SRCNN
@@ -38,11 +39,14 @@ class MyDataGenerator(object):
                     yield x_inputs, x_labels
 
     def load_image(self, src_dir, f):
-        X = np.asarray(Image.open(join(src_dir, f)).convert('RGB'), dtype='float32')
+        img = Image.open(join(src_dir, f))
+        X = np.asarray(img.convert('YCbCr'), dtype='float32')
+        X = X[:,:,0]
         X /= 255.
+        X = np.reshape(X,(X.shape[0], X.shape[1], 1))
         return X
 
-def train(log_dir, model_dir, train_dir, test_dir, eval_img):
+def train(log_dir, model_dir, train_dir, test_dir, epochs, steps, sync):
     srcnn_model = model()
     print(srcnn_model.summary())
 
@@ -57,11 +61,6 @@ def train(log_dir, model_dir, train_dir, test_dir, eval_img):
         os.path.join(test_dir, 'label'),
         batch_size = 10)
 
-    class PredCallback(Callback):
-        def on_epoch_end(self, epoch, logs=None):
-            pass
-            #pred.predict(self.model, eval_img, 'base-%d.png' % epoch, 'out-%d.png' % epoch, False)
-
     class PSNRCallback(Callback):
         def on_epoch_end(self, epoch, logs=None):
             loss = logs['loss'] * 255.
@@ -72,18 +71,17 @@ def train(log_dir, model_dir, train_dir, test_dir, eval_img):
             print("PSNR:%s" % psnr)
             print("PSNR(val):%s" % val_psnr)
 
-    pd_cb = PredCallback()
     ps_cb = PSNRCallback()
     md_cb = ModelCheckpoint(os.path.join(model_dir,'check.h5'), monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='min', period=1)
     tb_cb = TensorBoard(log_dir=log_dir)
 
     srcnn_model.fit_generator(
         generator = train_gen,
-        steps_per_epoch = 100,
+        steps_per_epoch = steps,
         validation_data = val_gen,
-        validation_steps = 100,
-        epochs = 100,
-        callbacks=[pd_cb, ps_cb, md_cb, tb_cb])
+        validation_steps = steps,
+        epochs = epochs,
+        callbacks=[ps_cb, md_cb, tb_cb])
 
     srcnn_model.save(os.path.join(model_dir,'model.h5'))
 
@@ -94,11 +92,13 @@ if __name__ == "__main__":
     parser.add_argument("model_dir")
     parser.add_argument("train_dir")
     parser.add_argument("test_dir")
-    parser.add_argument("--eval_img")
+    parser.add_argument("-epochs", type=int, default=100)
+    parser.add_argument("-steps", type=int, default=100)
+    parser.add_argument("-sync", type=bool, default=False)
     args = parser.parse_args()
     print(args)
 
     if not exists(args.model_dir):
         makedirs(args.model_dir)
 
-    train(args.log_dir, args.model_dir, args.train_dir, args.test_dir, args.eval_img)
+    train(args.log_dir, args.model_dir, args.train_dir, args.test_dir, args.epochs, args.steps, args.sync)
