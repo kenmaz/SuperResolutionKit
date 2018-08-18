8cbda722f3c84aeb7267c585dd60211292f2ce23
kenmaz <kentaro.matsumae@gmail.com>
2018-08-18 20:43:30 +0900

    add closure

--- SuperResolutionKit/Classes/UIImageView+SRCNN.swift
+++ SuperResolutionKit/Classes/UIImageView+SRCNN.swift
@@ -10,23 +10,34 @@ import UIKit
 extension UIImageView {
     
     public func setSRImage(image src: UIImage) {
+        setSRImage(image: src, completion: {})
+    }
+    
+    public func setSRImage(image src: UIImage, completion: @escaping (() -> Void)) {
         self.image = src
         DispatchQueue.global().async { [weak self] in
             if let output = SRCNNConverter.shared.convert(from: src) {
                 DispatchQueue.main.async {
                     self?.image = output
                     self?.layer.add(CATransition(), forKey: nil)
+                    completion()
                 }
             }
         }
     }
+    
     public func setFSRImage(image src: UIImage) {
+        setFSRImage(image: src, completion: {})
+    }
+    
+    public func setFSRImage(image src: UIImage, completion: @escaping (() -> Void)) {
         self.image = src
         DispatchQueue.global().async { [weak self] in
             if let output = FSRCNNConverter.shared.convert(from: src) {
                 DispatchQueue.main.async {
                     self?.image = output
                     self?.layer.add(CATransition(), forKey: nil)
+                    completion()
                 }
             }
         }
